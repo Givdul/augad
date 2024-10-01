1dd8c308101c435feb97e6697b74a6a546194ebf
Ludvig Budalen Hansen <ludvigha@elkjop.no>
2024-10-01 13:01:18 +0200

    Updates

---
+++ _next/static/WJkPyLETvSjUSB834_QEi/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-1be831200e60c5c0.js"],"/ar":["static/chunks/pages/ar-ef1bf2fa60643dc4.js"],sortedPages:["/_app","/_error","/ar"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
